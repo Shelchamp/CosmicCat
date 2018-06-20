ffc4ebef6bcc993413a40e55dad0722e9dd91dc7
Sheldon <Sheld.Chan@gmail.com>
2018-06-19 17:13:32 -0700

    adjust canvas dimensions

--- javascripts/game.js
+++ javascripts/game.js
@@ -1,9 +1,11 @@
 class Game {
   constructor(){
-
+    this.allFish = []
   }
 
-
+  addFish(fish){
+    this.allFish.push(this.randomPositon(fish))
+  }
 
   randomPositon(fish){
     let random_y = Math.random() * Game.DIM_Y;
@@ -13,16 +15,17 @@ class Game {
       case 0:
         fish.pos = [0, random_y];
         fish.vel = Math.abs(fish.vel);
-        break;
+        return fish;
 
       case 1:
         fish.pos = [Game.DIM_X, random_y];
-        fish.vel = -fish.vel
-        break;
+        fish.vel = -fish.vel;
+        return fish;
 
       default:
         fish.pos = [Game.DIM_X, random_y];
         fish.vel = -fish.vel;
+        return fish;
     }
   }
 
